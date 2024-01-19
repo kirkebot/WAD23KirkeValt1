d8c9e83b2309306ec5eb9fcb73a64fd9d542d8c3
kirkebot <65442670+kirkebot@users.noreply.github.com>
2024-01-19 15:04:07 +0200

    Task4

--- src/views/Students.vue
+++ src/views/Students.vue
@@ -19,12 +19,16 @@
             <td>{{ grade.hw1 }}</td>
             <td>{{ grade.hw2 }}</td>
             <td>{{ grade.exam }}</td>
-            <td :style="{background: grade.final <= 20 || grade.exam <= 20 ? 'red' : 'green' }">
+            <td :style="{ background: grade.final <= 50 || grade.exam <= 20 ? 'red' : 'green' }" @click="showJustification(grade)">
               {{ grade.final }}
             </td>
           </tr>
         </tbody>
       </table>
+    </div>
+
+    <div class="justification" v-if="justificationVisible">
+      <p :style="{ background: justificationBackgroundColor }">{{ justificationText }}</p>
     </div>
   </div>
 </template>
@@ -35,6 +39,9 @@ export default {
   data() {
     return {
       grades: [],
+      justificationVisible: false,
+      justificationText: "",
+      
     };
   },
   methods: {
@@ -43,6 +50,20 @@ export default {
         .then((response) => response.json())
         .then((data) => (this.grades = data))
         .catch((err) => console.log(err.message));
+    },
+    showJustification(grade) {
+      if (grade.final <= 50 && grade.exam <= 20) {
+        this.justificationText = "Grade justification: You failed because you need to get 21 points or more in the exam to pass the course and you also need 51 points or more in the HWs and exam to pass the course.";
+      } else if(grade.final <= 50 && grade.exam > 20){
+        this.justificationText = "Grade justification: You failed because you need to get 51 or more points in the HWs and exam to pass the course.";
+      } else if(grade.final > 50 && grade.exam <= 20){
+        this.justificationText = "Grade justification: You failed because you need to get 21 or more points in the exam to pass the course.";
+      } else if(grade.final > 50 && grade.exam > 20){
+        this.justificationText = "Grade justification: You have passed because you got over 20 points in the exam and over 50 points in the HWs and exam!";
+      }
+        
+
+      this.justificationVisible = true;
     },
   },
   mounted() {
@@ -78,8 +99,18 @@ th, td {
 th {
   background-color: brown;
 }
-tbody{
-  background-color: #E2A499;
+tbody {
+  background-color: #e2a499;
   color: black;
+}
+.justification {
+  background: rgb(16, 153, 194);
+  margin-bottom: 30px;
+  margin-top: 30px;
+  padding: 10px 20px;
+  margin: auto;
+  width: 80%;
+  display: flex;
+  justify-content: center;
 }
 </style>
