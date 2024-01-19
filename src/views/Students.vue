c8580c68cebd8920f81270db3a1d735ac533686f
kirkebot <65442670+kirkebot@users.noreply.github.com>
2024-01-19 14:35:01 +0200

    Task2

--- src/views/Students.vue
+++ src/views/Students.vue
@@ -4,7 +4,7 @@
 
     <div class="container">
       <table>
-        
+        <thead>
           <tr>
             <th>St. Code</th>
             <th>Homework 1</th>
@@ -12,8 +12,8 @@
             <th>Exam</th>
             <th>Final grade</th>
           </tr>
-        
-        
+        </thead>
+        <tbody>
           <tr v-for="grade in grades" :key="grade.id">
             <td>{{ grade.studentcode }}</td>
             <td>{{ grade.hw1 }}</td>
@@ -21,7 +21,7 @@
             <td>{{ grade.exam }}</td>
             <td>{{ grade.final }}</td>
           </tr>
-        
+        </tbody>
       </table>
     </div>
   </div>
@@ -65,9 +65,19 @@ h1 {
 }
 table {
   width: 100%;
-  
   margin-top: 20px;
 }
-
-
+th, td {
+  border: 3px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+  color: black;
+}
+th {
+  background-color: brown;
+}
+tbody{
+  background-color: #E2A499;
+  color: black;
+}
 </style>
